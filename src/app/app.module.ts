849c72bb5edd5e71c6b05900df07ca6a1060b4dd
james.mugo@ajiraconnect.com <james.mugo@ajiraconnect.com>
2020-08-08 12:04:34 +0300

    add nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/events-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
+    NavBarComponent,
     EventsListComponent,
     EventThumbnailComponent
   ],
