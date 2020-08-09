832008632d3582cd4a8d9f788b6a917a187559e3
james.mugo@ajiraconnect.com <james.mugo@ajiraconnect.com>
2020-08-09 17:51:34 +0300

    hidding and showing components with ngSwitch

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -5,7 +5,11 @@ import { Component,Input, Output,EventEmitter } from '@angular/core'
     <div class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}} </div>
-      <div>Time: {{event?.time}} </div>
+      <div [ngSwitch]="event?.time">Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
       <div>Price:  \${{event?.price}}</div>
       <div>
       <span>Location: {{event?.location.address}}</span>
@@ -17,7 +21,7 @@ import { Component,Input, Output,EventEmitter } from '@angular/core'
   `,
 
   styles: [`
-    .thumbnail { min-height:210px;}
+    .thumbnail { min-height:250px;}
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb;}
     `]
